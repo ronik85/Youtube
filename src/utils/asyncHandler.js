c74743c57e81c3b0da5ce7add25d2fd7bf2499cb
Ronik <ronik.kalanouria@gmail.com>
2024-05-01 19:02:44 +0530

    All setup configured

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,7 +1,6 @@
-const asyncHandler = async (requestHandler) => {
-  (req, res, next) => {
-    Promise.resolve(requestHandler(req, res, next)).
-    catch((err) => next(err));
+const asyncHandler = (requestHandler) => {
+  return (req, res, next) => {
+    Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
   };
 };
 
